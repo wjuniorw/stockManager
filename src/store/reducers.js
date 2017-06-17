85ff50ae64487d3fde545563cb1d4d7b515992fa
wjuniorw <wilson.junior90@gmail.com>
2017-06-17 12:14:29 -0300

    add reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,10 @@
 // import reducers
 // import categorys from './cat/reducer'
+import caixa from './caixa/reducer'
+import products from './products/reducer'
 
 // export all reducers
 export {
-
+  caixa,
+  products
 }
