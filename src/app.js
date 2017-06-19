79bc8d03458572d972b0e68e7f2c3d14cd5a7cc7
wjuniorw <wilson.junior90@gmail.com>
2017-06-19 18:21:41 -0300

    ADD styles

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import * as types from './store/products/actionTypes'
 import * as productsSelector from './store/products/reducer'
 // import containers...
 
-
 class App extends Component {
   constructor(props) {
     super(props)
@@ -21,19 +20,40 @@ class App extends Component {
     return (
       <div>
         <h1>Stock Manager</h1>
-        <input type="text" ref="name" placeholder="nome producto" />
-        <br />
-        <input type="button" value="novo producto" onClick={this.newProduct} />
-        <br />
-        <h3>Lista de productos</h3>
-        <h4>
-        <ul>
+
+        <div className="container-fluid">
+        <div className="new-prod">
+        <div className="card col-md-offset-6">
+          <div className="body">
+            <input className="form-control" type="text" ref="name" placeholder="nome producto" />
+            <br />
+            <a className="btn btn-primary" onClick={this.newProduct}>Novo Produto</a>
+            <br />
+          </div>
+        </div>
+        </div>
+
+        <div className="card contentProd">
+        <div className="info-container">
           {
-            _.map(this.props.products.products, this.renderProd)
+            _.map(this.props.products.products, this.renderBox)
           }
-        </ul>
-        </h4>
+        </div>
+        </div>
+        <div className="card">
+          <h3 className="header">Lista de productos</h3>
+          <h4>
+            Teste...
+          </h4>
+          <ul>
+            {
+              _.map(this.props.products.products, this.renderProd)
+            }
+          </ul>
+        </div>
       </div>
+
+    </div>
     )
   }
   newProduct() {
@@ -45,7 +65,8 @@ class App extends Component {
     }
     else {
       let current = this.localProd('prod')
-      current.push(this.refs.name.value)
+      current.unshift(this.refs.name.value)
+      // current.push(this.refs.name.value)
       console.log('log--0', current)
       let up = this.upDateprod('prod', current)
       console.log('log-t', up)
@@ -62,6 +83,21 @@ class App extends Component {
                 {item}
               </li>
             )
+  }
+  renderBox(item, i) {
+    return (
+      <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12" key={i}>
+        <div className="info-box-3 bg-indigo hover-expand-effect">
+          <div className="icon">
+            <i className="material-icons">{item}</i>
+              </div>
+              <div className="content">
+                <div className="text">NEW PRODUCT</div>
+                <div className="number count-to" data-from="0" data-to="{i}" data-speed="1000" data-fresh-interval="20">{i + 1}</div>
+              </div>
+        </div>
+      </div>
+    )
   }
   prodToJson(str) {
     return JSON.parse(str)
--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import App from './app'
 
+//import styles...
+import './styles/bootstrap.css'
+import './styles/style.css'
+import './styles/index.css'
+
 import * as reducers from './store/reducers'
 const store = createStore(combineReducers(reducers), applyMiddleware(thunk))
 
